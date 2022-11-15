88ec02d277574a94da32b78b9b2ecf148e534de1
Kenneth Dong <dongkenneth256@gmail.com>
2022-11-15 17:29:35 -0500

    Update parseTranscript.js

--- frontend/src/utils/parseTranscript.js
+++ frontend/src/utils/parseTranscript.js
@@ -1,5 +1,6 @@
-var JSSoup = require('jssoup').default;
+// var JSSoup = require('jssoup').default;
 
-function parseTranscript(transcript) {
-    const soup = new JSSoup(transcript);
-    const paragraphs = soup.findAll('p');}
+export function parseTranscript(transcript) {
+  const soup = new JSSoup(transcript);
+  const paragraphs = soup.findAll('p');
+};
