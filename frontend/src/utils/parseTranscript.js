27dcf9838d166aa857fc376410b0265cdd5e90c7
Kenneth Dong <dongkenneth256@gmail.com>
2022-11-29 17:33:03 -0500

    Update parseTranscript.js

--- frontend/src/utils/parseTranscript.js
+++ frontend/src/utils/parseTranscript.js
@@ -2,5 +2,6 @@
 export function parseTranscript(transcript) {
   var arrayOfData = transcript.split("STUDENT INFORMATION");
   var info = arrayOfData[1];
-  console.log(info);
+  var splitInfo = info.split("<!--  ** START OF twbkwbis.P_CloseDoc **  -->");
+  console.log(splitInfo[0]);
 };
