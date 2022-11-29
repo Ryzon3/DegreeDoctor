dd2d8210582fd8b4039f25642598a2a09fd7db48
Kenneth Dong <dongkenneth256@gmail.com>
2022-11-29 17:41:34 -0500

    working on transcript

--- frontend/src/utils/parseTranscript.js
+++ frontend/src/utils/parseTranscript.js
@@ -1,7 +1,14 @@
 
-export function parseTranscript(transcript) {
+export function parseTranscript(data) {
+  var transcript = data.replace('&amp','&');
+  console.log(transcript);
   var arrayOfData = transcript.split("STUDENT INFORMATION");
   var info = arrayOfData[1];
   var splitInfo = info.split("<!--  ** START OF twbkwbis.P_CloseDoc **  -->");
-  console.log(splitInfo[0]);
+  var majors = splitInfo[0];
+  var yourMajors = majors.split("<th colspan=\"3\" class=\"ddlabel\" scope=\"row\">Major and Department:</th>") 
+  for (let i = 0; i < yourMajors.length; i++){
+    console.log(yourMajors[i]);
+    console.log("\n\n\n");
+  }
 };
