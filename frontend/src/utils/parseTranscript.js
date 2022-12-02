02600b3df147f07096d6a0bf2617c999ffe8fa51
Kenneth Dong <dongkenneth256@gmail.com>
2022-12-02 16:46:49 -0500

    added logic to get majors

--- frontend/src/utils/parseTranscript.js
+++ frontend/src/utils/parseTranscript.js
@@ -1,14 +1,19 @@
 
 export function parseTranscript(data) {
-  var transcript = data.replace('&amp','&');
-  console.log(transcript);
-  var arrayOfData = transcript.split("STUDENT INFORMATION");
-  var info = arrayOfData[1];
-  var splitInfo = info.split("<!--  ** START OF twbkwbis.P_CloseDoc **  -->");
-  var majors = splitInfo[0];
-  var yourMajors = majors.split("<th colspan=\"3\" class=\"ddlabel\" scope=\"row\">Major and Department:</th>") 
-  for (let i = 0; i < yourMajors.length; i++){
-    console.log(yourMajors[i]);
-    console.log("\n\n\n");
+  var el = document.createElement('html');
+  el.innerHTML = data;
+  var majors = new Array(10);
+  var a = el.getElementsByTagName('th');
+  var b = el.getElementsByTagName('td'); 
+  var i = 6;
+  while(true){
+    if (a[i].innerText == "Major and Department:") {
+      console.log(b[i+20]);
+      i++;
+    }
+    else{
+      break;
+    }
   }
+
 };
